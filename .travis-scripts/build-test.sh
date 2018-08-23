1618324d357bd9df0ffdafbf471818978d4f175a
amete <serhanmete@gmail.com>
2018-08-23 14:24:36 +0200

    Testing...

--- .travis-scripts/build-test.sh
+++ .travis-scripts/build-test.sh
@@ -1,5 +1,3 @@
-cd /tmp
-
 if [ -z "$CXX" ]; then
     CXX=$(type -p g++)
 fi
@@ -12,6 +10,5 @@ if [ -z "$CMAKE" ]; then
   CMAKE=$(type -p cmake)
 fi
 
-$CMAKE -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_C_COMPILER=$CC /mnt
-
+$CMAKE -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_C_COMPILER=$CC ..
 make -j
