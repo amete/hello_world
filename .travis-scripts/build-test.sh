06a39f1071184e3261aa016fd36b3ed9e578c5b8
amete <serhanmete@gmail.com>
2018-08-23 14:05:19 +0200

    Add basic Travis CI files

---
+++ .travis-scripts/build-test.sh
@@ -0,0 +1,17 @@
+cd /tmp
+
+if [ -z "$CXX" ]; then
+    CXX=$(type -p g++)
+fi
+
+if [ -z "$CC" ]; then
+  CC=$(type -p gcc)
+fi
+
+if [ -z "$CMAKE" ]; then
+  CMAKE=$(type -p cmake)
+fi
+
+$CMAKE -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_C_COMPILER=$CC /mnt
+
+make -j
