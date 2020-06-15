477abb5d49b4b3a27d855dee8600eafa3898d81e
Alaettin Serhan Mete <serhanmete@gmail.com>
2020-06-15 16:55:09 +0200

    Messing around

--- src/hello_world.cxx
+++ src/hello_world.cxx
@@ -1,7 +1,3 @@
 #include<iostream>
  
-
-int main(int argc, char *argv[]){
-   std::cout << "Hello World!" << std::endl;
-   return 0;
-}
+int main(int argc, char *argv[]){ std::cout << "Hello World!" << std::endl; return 0; }
